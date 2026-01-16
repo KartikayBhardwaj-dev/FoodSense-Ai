fc370efc860f64fce617a9368d5ae3b99ed6c5b7
kartikayy-01 <kartikayb1405@gmail.com>
2026-01-16 15:54:41 +0530

    deploy ready

--- test.py
+++
@@ -1,9 +0,0 @@
-from src.models.model import load_model
-from src.data.preprocess import preprocess_image
-from src.prediction.predict import predict_images
-
-model, device = load_model("artifacts/finetuned_resnet18.pth")
-label, confidence = predict_images("/Users/kartikaybhardwaj/FoodSense-Ai/Screenshot 2026-01-16 at 11.19.29 AM.png", model, device)
-
-print("prediction: ", label)
-print("Confidence: ", confidence)
